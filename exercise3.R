e288e81996b947dd4ddd3bee58a32b0c0ddfc10d
haijunXue <xuehaijun793g@gmail.com>
2017-11-02 17:34:37 +0100

    commit

--- exercise3.R
+++ exercise3.R
@@ -3,10 +3,12 @@
 #(2)git commit -m "remove Exercise"
 #(3)git push origin master
 
-
+#touch README
+#git add README
 #git rm HaijunXue_solution
 #git commit -m "remove"
 #git push origin mater
+#git reset --hard origin/master
 
 #how to upload data to git
 #(1)git init
@@ -14,3 +16,23 @@
 #(3)git commit -m "add all files"
 #(4) git remote add github https://github.com/compstat-lmu/ws1718_innolab_for_students/HaijunXue_solution
 #(5)git push -f github  master
+
+
+
+#3.
+#improve this script by wrapping the printer insider a funciton 
+
+
+
+#branch
+#1.git branch  -------------check branch
+#2. git checkout -b iss53          --- create a branch and switch to it at the same time
+#  git branch iss53
+# git checkout iss53         -- the same funktion
+
+
+printer <- function(name,eyecolor,favorite.meal){
+  print(paste0(name," has ", eyecolor," eyes and enjoys eating ",meal, " the most!"))
+}
+
+printer(name,eyecolor,meal)
---
+++ printme.R
@@ -0,0 +1,27 @@
+name = "haijun"
+eyecolor = "brown"
+meal = "fish"
+print(paste0(name," has  ", eyecolor," eyes and enjoys eating ",meal, " the most!"))
+
+#(2)
+
+
+#---4.3
+# improve this script by wrapping  the printer inside a function 
+# printer(name,eyecolor,favorite.meal)
+
+#(1) create a branch
+# git branch testing
+
+#----4.4
+# git checkout that branch ---switch to it
+# git checkout testing
+
+
+#---4.5
+#implement your changes(the new function on the bottom of the script) in the branch.
+# Add,commit and push them to the branch to Github
+
+
+
+
