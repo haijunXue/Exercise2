f740a3f4d2aa6207be7e342a8f78b31964769080
haijunXue <xuehaijun793g@gmail.com>
2017-11-02 12:45:47 +0100

    commit

---
+++ exercise3.R
@@ -0,0 +1,16 @@
+#how to remove data from git
+#(1)git rm -r  Exercise.R
+#(2)git commit -m "remove Exercise"
+#(3)git push origin master
+
+
+#git rm HaijunXue_solution
+#git commit -m "remove"
+#git push origin mater
+
+#how to upload data to git
+#(1)git init
+#(2)git add -A
+#(3)git commit -m "add all files"
+#(4) git remote add github https://github.com/compstat-lmu/ws1718_innolab_for_students/HaijunXue_solution
+#(5)git push -f github  master
